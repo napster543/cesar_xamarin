3f2e1526ad82dc12244d39bf235502d764389f08
napster543 <napster543@gmail.com>
2018-07-03 23:20:18 -0500

    Login page
    Primer push

---
+++ cesar_xamarin/Cesar_xamarin/Cesar_xamarin/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿ namespace Cesar_xamarin.Infrastructure
+{
+    using Xamarin.Forms;
+    using ViewModels;
+    public class InstanceLocator 
+    {        
+            #region Property
+            public MainViewModel Main
+            {
+                get;
+                set;
+            }
+            #endregion
+
+            #region constructor
+            public InstanceLocator()
+            {
+                this.Main = new MainViewModel();
+            }
+            #endregion
+        }
+    
+
+}
---
+++ cesar_xamarin/Cesar_xamarin/Cesar_xamarin/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Cesar_xamarin.ViewModels
+{
+    public class MainViewModel : ContentPage
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
