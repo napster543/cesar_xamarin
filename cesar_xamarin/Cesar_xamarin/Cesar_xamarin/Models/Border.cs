101de1d7bd51bbc168b096f16d60bc85e23ef42a
napster543 <napster543@gmail.com>
2018-07-18 18:01:37 -0500

    Translation Page

--- cesar_xamarin/Cesar_xamarin/Cesar_xamarin/Models/Border.cs
+++ cesar_xamarin/Cesar_xamarin/Cesar_xamarin/Models/Border.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// https://gist.github.com/ads90/49cdea7d609a5b364eb34c9feedb7271
+// https://stackoverrun.com/es/q/11337168/xamarin-forms-stacklayout-con-esquinas-redondeadas
+// http://venkyxamarin.blogspot.com/2017/12/how-to-set-corner-radius-for-view.html#more
+// https://github.com/andreinitescu/Xamore/blob/master/Xamore.Controls/Border.cs
 namespace Cesar_xamarin.Models
 {
     public class Border
--- cesar_xamarin/Cesar_xamarin/Cesar_xamarin/Models/Translations.cs
+++ cesar_xamarin/Cesar_xamarin/Cesar_xamarin/Models/Translations.cs
@@ -23,6 +23,6 @@ namespace Cesar_xamarin.Models
         [JsonProperty(PropertyName = "hr")]
         public string Croatian { get; set; }
         [JsonProperty(PropertyName = "fa")]
-        public string Danish { get; set; }
+        public string Persian { get; set; }
     }
 }
